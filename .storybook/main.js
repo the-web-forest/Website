981c3c4a5928e0e2c1f7c1a66950dd52b04e148e
Barros <mdbf42@gmail.com>
2021-12-18 22:34:23 -0300

    Added Controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../stories/**/*.stories.js', '../stories/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/preset-scss'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/preset-scss', '@storybook/addon-controls'],
   presets: [path.resolve(__dirname, './next-preset.js')],
   framework: "@storybook/react"
 };
