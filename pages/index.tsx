880d08440fa07e9519737cdbaacb2ca5a2113236
DevBacega <dev.victor.bacega@gmail.com>
2022-01-24 23:22:26 -0300

    Inserindo a seção do banner na Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import DonateButton from '../components/FilledButton'
 import Header from '../components/header'
+import BannerSection from '../sections/Banner'
 import HeaderSection from '../sections/Header'
+import HowItWork from '../sections/HowItWork'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -10,6 +12,7 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Header/>
       <HeaderSection/>
+      <BannerSection/>
     </div>
   )
 }
