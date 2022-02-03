f7adcde8e4a3d9c8d79c1db4e0c7583e30443274
Barros <mdbf42@gmail.com>
2022-02-03 00:04:30 -0300

    News Section

--- sections/News/index.tsx
+++ sections/News/index.tsx
@@ -14,33 +14,30 @@ const NewsSection = () => {
             <div id={styles.container}>
                 <div id={styles.title}>O que está acontecendo com <br /> <u>o meio ambiente?</u> </div>
             </div>
-
             <CarouselProvider
-            naturalSlideWidth={100}
-            naturalSlideHeight={125}
-            totalSlides={news.length}
-            infinite={true}
-            visibleSlides={4}
-            className={styles.carousel}
-            interval={5000}
-            isPlaying={true}
-            isIntrinsicHeight={true}
-        >
-            <Slider>
-            { news.map((x, i) => 
-                <Slide key={i} index={i}>
-                    <NewsCard
-                      title={x.title}
-                      date={x.date}
-                      newsUrl={x.link}
-                      photoUrl={x.photoUrl}
-                    />
-                </Slide>
-            ) }
-               
-            </Slider>
-        </CarouselProvider>
-
+                naturalSlideWidth={100}
+                naturalSlideHeight={125}
+                totalSlides={news.length}
+                infinite={true}
+                visibleSlides={4}
+                className={styles.carousel}
+                interval={5000}
+                isPlaying={true}
+                isIntrinsicHeight={true}
+            >
+                <Slider>
+                { news.map((x, i) => 
+                    <Slide key={i} index={i}>
+                        <NewsCard
+                        title={x.title}
+                        date={x.date}
+                        newsUrl={x.link}
+                        photoUrl={x.photoUrl}
+                        />
+                    </Slide>
+                ) }
+                </Slider>
+            </CarouselProvider>
         </>
     )
 }
