e29b0096b3f0acb896104e613619754c7a67a7b0
Barros <mdbf42@gmail.com>
2022-02-28 18:35:35 -0300

    Env Var

--- components/header.tsx
+++ components/header.tsx
@@ -54,6 +54,7 @@ const Header = (props: HeaderData) => {
             <meta property="og:description" content="Web Forest - Uma floresta virtual que transforma ações digitais em impactos reais"/>
             <meta property="og:type" content="website"/>
             <meta property="og:image" content="/images/open-graph.png"/>
+            {renderTags()}
         </Head>
     )
 
