38522e9f3264382c7a2fa7b1020ed639a5c77aa5
Barros <mdbf42@gmail.com>
2022-03-23 22:15:40 -0300

    Fix Cors

--- services/NewsService.ts
+++ services/NewsService.ts
@@ -3,11 +3,7 @@ import News from '../core/domain/News';
 
 export default class NewsService {
   async getAllNews(): Promise<News[]> {
-    const news = await (
-      await axios.get(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/handlers/news/list`,
-      )
-    ).data;
+    const news = await (await axios.get(`/api/handlers/news/list`)).data;
     return news.map((n: any) => {
       return new News({
         date: n.date,
