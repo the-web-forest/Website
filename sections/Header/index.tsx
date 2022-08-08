cd9c80ec9c4afe8492ed7753fe4c17ed5b897e8e
Décio Stenico <devstenico@gmail.com>
2022-08-07 22:18:59 -0300

    Removendo HTML desnecessário

--- sections/Header/index.tsx
+++ sections/Header/index.tsx
@@ -13,86 +13,83 @@ const HeaderSection = () => {
   };
 
   return (
-    <>
-      <a id="inicio" tabIndex={-1}></a>
-      <div className={styles.header}>
-        <header>
-          <div className={styles.container}>
-            <div className={styles.headerInner}>
-              <div className={styles.headerLogo}>
-                <a href={Settings.APP_URL} className={styles.headerLogoLink}>
-                  <img
-                    src="images/logo.svg"
-                    title={`Logo ${Settings.APP_NAME}`}
-                    className={styles.headerLogoImage}
-                  />
-                </a>
-              </div>
-
-              <div
-                className={[
-                  styles.headerMenu,
-                  isMenuOpen ? styles.isMenuOpen : '',
-                ].join(' ')}
-              >
-                <button
-                  className={styles.headerMenuMobile}
-                  onClick={toggleMenu}
-                  tabIndex={-1}
-                >
-                  {isMenuOpen ? <HiX size={24} /> : <HiMenu size={24} />}
-                </button>
-                <nav className={styles.menuOptionsNav}>
-                  <ul className={styles.menuOptions}>
-                    <HeaderButton
-                      text="Início"
-                      onClick={() => {
-                        gaButtonClick('inicio');
-                        setIsMenuOpen(false);
-                      }}
-                      selected
-                      url={`#`}
-                    />
-                    <HeaderButton
-                      text="Apoie"
-                      onClick={() => {
-                        gaButtonClick('apoie');
-                        setIsMenuOpen(false);
-                      }}
-                      url={`#support`}
-                    />
-                    <HeaderButton
-                      text="Quem Somos"
-                      onClick={() => {
-                        gaButtonClick('quem-somos');
-                        setIsMenuOpen(false);
-                      }}
-                      url={`#team`}
-                    />
-                    <HeaderButton
-                      text="Contato"
-                      onClick={() => {
-                        gaButtonClick('contato');
-                        setIsMenuOpen(false);
-                      }}
-                      url={`#contact`}
-                    />
-                  </ul>
-                </nav>
-              </div>
-
-              <div className={styles.headerTrailing}>
-                <DonateButton
-                  text="Faça uma Doação"
-                  url={Settings.DONATE_URL}
-                  target="_blank"
+    <div className={styles.header}>
+      <header>
+        <div className={styles.container}>
+          <div className={styles.headerInner}>
+            <div className={styles.headerLogo}>
+              <a href={Settings.APP_URL} className={styles.headerLogoLink}>
+                <img
+                  src="images/logo.svg"
+                  title={`Logo ${Settings.APP_NAME}`}
+                  className={styles.headerLogoImage}
                 />
-              </div>
+              </a>
+            </div>
+
+            <div
+              className={[
+                styles.headerMenu,
+                isMenuOpen ? styles.isMenuOpen : '',
+              ].join(' ')}
+            >
+              <button
+                className={styles.headerMenuMobile}
+                onClick={toggleMenu}
+                tabIndex={-1}
+              >
+                {isMenuOpen ? <HiX size={24} /> : <HiMenu size={24} />}
+              </button>
+              <nav className={styles.menuOptionsNav}>
+                <ul className={styles.menuOptions}>
+                  <HeaderButton
+                    text="Início"
+                    onClick={() => {
+                      gaButtonClick('inicio');
+                      setIsMenuOpen(false);
+                    }}
+                    selected
+                    url={`#`}
+                  />
+                  <HeaderButton
+                    text="Apoie"
+                    onClick={() => {
+                      gaButtonClick('apoie');
+                      setIsMenuOpen(false);
+                    }}
+                    url={`#support`}
+                  />
+                  <HeaderButton
+                    text="Quem Somos"
+                    onClick={() => {
+                      gaButtonClick('quem-somos');
+                      setIsMenuOpen(false);
+                    }}
+                    url={`#team`}
+                  />
+                  <HeaderButton
+                    text="Contato"
+                    onClick={() => {
+                      gaButtonClick('contato');
+                      setIsMenuOpen(false);
+                    }}
+                    url={`#contact`}
+                  />
+                </ul>
+              </nav>
+            </div>
+
+            <div className={styles.headerTrailing}>
+              <DonateButton
+                text="Faça uma Doação"
+                url={Settings.DONATE_URL}
+                target="_blank"
+              />
             </div>
           </div>
-        </header>
-      </div>
-    </>
+        </div>
+      </header>
+    </div>
   );
 };
 
