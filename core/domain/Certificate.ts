12467a238dff5cb8d8e95c06fffef276ab167967
hiagoindalecio <hiagoindalecio@outlook.com>
2022-10-25 22:23:54 -0300

    Feature/certificate screen (#102)
    * Creating the new screen
    
    * Initiating the certificate validation screen styles development
    
    * Finding certificate and showing feedback
    
    * Finishing the certicate validation screen layout
    
    * Configuring the environment variables
    
    * Finishing the no content typed validation (certificate validation)

---
+++ core/domain/Certificate.ts
@@ -0,0 +1,11 @@
+export default class Certificate {
+  id!: string;
+  name!: string;
+  certificateUrl!: string;
+  createtAt!: Date;
+  updatedAt!: Date;
+
+  constructor(data: Partial<Certificate>) {
+    Object.assign(this, data);
+  }
+}
