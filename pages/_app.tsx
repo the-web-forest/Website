8cd88eb7038d7d987e59b61af2f64ac47f0f012d
barros <mdbf42@gmail.com>
2022-10-26 00:24:21 -0300

    Feature @ HotJar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3218769, 6);
+  }, []);
+
+  // @ts-ignore
   return <Component {...pageProps} />;
 }
 
